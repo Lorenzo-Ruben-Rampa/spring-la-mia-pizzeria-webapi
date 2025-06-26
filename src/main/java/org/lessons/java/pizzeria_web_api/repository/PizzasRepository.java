831a527db79a09cc26f9eaeaeeaa63bea3d6245c
Lorenzo-Ruben-Rampa <lorenzo.rampa.ruben@gmail.com>
2025-06-26 09:07:29 +0200

    implementazione service

--- src/main/java/org/lessons/java/pizzeria_web_api/repository/PizzasRepository.java
+++ src/main/java/org/lessons/java/pizzeria_web_api/repository/PizzasRepository.java
@@ -6,4 +6,5 @@ import java.util.List;
 public interface PizzasRepository extends JpaRepository <Pizza, Integer> {
     // Nuovo metodo per cercare pizze il cui nome contiene una certa stringa (case-insensitive)
     List<Pizza> findByNameContainingIgnoreCase(String name);
+    List<Pizza> findByTitleContainingIgnoreCase(String title);
 }
---
+++ src/main/java/org/lessons/java/pizzeria_web_api/service/PizzaService.java
@@ -0,0 +1,77 @@
+package org.lessons.java.pizzeria_web_api.service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+import org.lessons.java.pizzeria_web_api.repository.PizzasRepository;
+import org.lessons.java.pizzeria_web_api.model.Ingredient;
+import org.lessons.java.pizzeria_web_api.model.Pizza;
+import java.util.List;
+import java.util.Optional;
+import org.lessons.java.pizzeria_web_api.repository.IngredientsRepository;
+
+
+@Service
+public class PizzaService {
+    @Autowired
+    private PizzasRepository pizzaRepository;
+
+    @Autowired
+    private IngredientsRepository ingredientsRepository;
+
+    public List<Pizza> findAll() {
+        return pizzaRepository.findAll();
+        }
+    public List<Pizza> findAllSortedByTitle() {
+        return pizzaRepository.findAll(Sort.by("title"));
+    }
+
+    public List<Pizza> findAllSortedByAuthor() {
+        return pizzaRepository.findAll(Sort.by("author"));
+    }
+
+    public Pizza getById(Integer id) {
+        Optional<Pizza> pizzaAttempt = pizzaRepository.findById(id);
+        if (pizzaAttempt.isEmpty()) {
+            //lancio una notFound con response entity
+               throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Pizza con ID " + id + " non trovata.");
+        }
+            return pizzaAttempt.get();
+        }
+
+        public List<Pizza> findByTitle(String title) {
+            return pizzaRepository.findByTitleContainingIgnoreCase(title);
+        }
+
+        public Pizza create(Pizza pizza) {
+            return pizzaRepository.save(pizza);
+        }
+
+        public Pizza update(Pizza pizza) {
+            return pizzaRepository.save(pizza);
+        }
+
+        public void delete(Pizza pizza) {
+            for (Ingredient ingredientToDelete : pizza.getIngredients()) {
+                ingredientsRepository.delete(ingredientToDelete);
+            }
+            pizzaRepository.delete(pizza);
+        }
+
+        public void deleteById(Integer id) {
+            Pizza pizza = getById(id);
+            for (Ingredient ingredientToDelete : pizza.getIngredients()) {
+                ingredientsRepository.delete(ingredientToDelete);
+            }
+            pizzaRepository.delete(pizza);
+        }
+
+        public Boolean existsById(Integer id) {
+            return pizzaRepository.existsById(id);
+        }
+
+        public Boolean exists(Pizza pizza) {
+            return existsById(pizza.getId());
+        }
+}
